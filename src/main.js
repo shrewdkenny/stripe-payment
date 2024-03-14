49f4cdf4c3fe494b271d711a8069700f2ab09d99
Shrewdkenny <shrewdkenny@gmail.com>
2024-03-15 00:04:11 +0100

    update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from "./router";
 import { createPinia } from "pinia";
 
 const app = createApp(App);
-app.use(createPinia())
+app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -5,7 +5,11 @@
       <p class="text-2xl font-black ml-[190px]">Price</p>
     </div>
 
-    <div class="flex flex-row mt-[50px] h-[10vh]" v-for="item in cartItems">
+    <div
+      class="flex flex-row mt-[50px] h-[10vh]"
+      v-for="item in cartItems"
+      :key="item.id"
+    >
       <div class="ml-[150px] w-[600px] text-2xl flex flex-row space-x-10">
         <img
           :src="item.pictures"
@@ -16,7 +20,8 @@
       </div>
       <p class="flex flex-row justify-center font-black">{{ item.price }}</p>
     </div>
-    <div class="flex flex-row justify-center items-center mb-10">
+    <div class="flex flex-col justify-center items-center mb-40">
+      <p class="text-2xl font-black ml-10 mt-10">Total: ${{ totalPrice }}</p>
       <button
         @click="pay()"
         class="bg-black text-white rounded-lg text-sm p-[10px] mt-[30px] w-[100px]"
@@ -29,23 +34,26 @@
 
 <script>
 import { useCart } from "@/stores/CartStore";
+
 export default {
   setup() {
     const cartStore = useCart();
     const cartItems = cartStore.items;
-    const productQuantity = cartStore.quantity;
+
     return {
       cartItems,
-      cartStore,
-      productQuantity,
     };
   },
   methods: {
     pay() {
       window.location.href = "https://buy.stripe.com/test_00g7w41uP6L7fhCbIK";
     },
-    increaseQuantity() {
-      this.productQuantity++;
+  },
+  computed: {
+    totalPrice() {
+      return this.cartItems.reduce((total, item) => {
+        return total + parseFloat(item.price.replace(/\$/g, ""));
+      }, 0);
     },
   },
 };
